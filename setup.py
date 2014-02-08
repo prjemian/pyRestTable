e610cdcffc7248562c60490d7eb3236f61a650f2
Pete Jemian <prjemian@gmail.com>
2014-02-08 13:27:59 -0600

    fix licensing comment

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-#-----------------------------------------------------------------------------
-# Copyright (c) 2014, Pete R. Jemian.
+
+# #-----------------------------------------------------------------------------
+# :version:   2014-02
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014, Pete R. Jemian
 #
-# Distributed under the terms of the -tba- License.
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 #-----------------------------------------------------------------------------
