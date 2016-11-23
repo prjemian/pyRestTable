3d69ed7783136275b8f1a374162557f0ecc5329e
Pete Jemian <prjemian@gmail.com>
2016-11-23 11:11:37 -0600

    help travis-ci

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -3,10 +3,11 @@
 unit testing of pyRestTable package
 '''
 
+import os
 import sys
 import unittest
 
-sys.path.insert(0, '..')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 import pyRestTable.rest_table
 
 
