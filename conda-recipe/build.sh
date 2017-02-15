b256354240300667684751efb65c0509bedcdff2
Pete Jemian <prjemian@gmail.com>
2017-02-15 08:20:18 -0600

    refs #10 initial conda recipe

---
+++ conda-recipe/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+$PYTHON setup.py install --single-version-externally-managed --record installed_files.txt
+
+# Add more build steps here, if they are necessary.
---
+++ conda-recipe/run_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os, sys, unittest
+import pyRestTable
+
+t = pyRestTable.Table()
+t.addLabel('City name')
+t.addLabel('Area')
+t.addLabel('Population')
+t.addLabel('Annual Rainfall')
+t.addRow(['Adelaide',  1295, 1158259,    600.5])
+t.addRow(['Brisbane',  5905, 1857594,    1146.4])
+t.addRow(['Darwin',    112,  120900,     1714.7])
+expected  = '========= ==== ========== ===============\n'
+expected += 'City name Area Population Annual Rainfall\n'
+expected += '========= ==== ========== ===============\n'
+expected += 'Adelaide  1295 1158259    600.5          \n'
+expected += 'Brisbane  5905 1857594    1146.4         \n'
+expected += 'Darwin    112  120900     1714.7         \n'
+expected += '========= ==== ========== ===============\n'
+assert(str(t) == expected)
