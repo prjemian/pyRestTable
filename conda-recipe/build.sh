4dfa0696121f262fd3a86ec77ffba159204a4b0d
Pete Jemian <prjemian@gmail.com>
2017-02-15 09:57:46 -0600

    fixes #10

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-$PYTHON setup.py install --single-version-externally-managed --record installed_files.txt
+$PYTHON setup.py install
+
+# $PYTHON setup.py install --single-version-externally-managed --record installed_files.txt
 
 # Add more build steps here, if they are necessary.
