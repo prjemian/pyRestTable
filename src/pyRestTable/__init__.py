bae7ac99fd011d9377c0e5b593b4f8a9dee1f8df
Pete Jemian <prjemian@gmail.com>
2019-05-08 12:45:46 -0500

    announce (additional) testing with py36 & py37

--- src/pyRestTable/__init__.py
+++ src/pyRestTable/__init__.py
@@ -51,6 +51,8 @@ __classifiers__ = [
      'Programming Language :: Python :: 2.7',
      'Programming Language :: Python :: 3',
      'Programming Language :: Python :: 3.5',
+     'Programming Language :: Python :: 3.6',
+     'Programming Language :: Python :: 3.7',
      'Topic :: Documentation',
      'Topic :: Documentation :: Sphinx',
      'Topic :: Software Development',
