51eadbf2c7bd3ec54f987ca6ebb9a4ad7013f34e
Pete R. Jemian <prjemian@gmail.com>
2020-11-09 14:16:40 -0600

    CI #37 build Py versions

--- src/pyRestTable/__init__.py
+++ src/pyRestTable/__init__.py
@@ -47,6 +47,7 @@ __classifiers__ = [
      'Programming Language :: Python :: 3.5',
      'Programming Language :: Python :: 3.6',
      'Programming Language :: Python :: 3.7',
+     'Programming Language :: Python :: 3.8',
      'Topic :: Documentation',
      'Topic :: Documentation :: Sphinx',
      'Topic :: Software Development',
