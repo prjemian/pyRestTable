c8763d51941e688fd932058a46608f9e8cd4b19a
Pete R Jemian <prjemian@gmail.com>
2022-08-26 01:34:17 -0500

    MNT #60 another possible exception

--- pyRestTable/__init__.py
+++ pyRestTable/__init__.py
@@ -8,7 +8,7 @@ try:
     from setuptools_scm import get_version
 
     __version__ = get_version()
-except ModuleNotFoundError:
+except (LookupError, ModuleNotFoundError):
     from importlib.metadata import version
 
     __version__ = version("pyRestTable")
