85b19190202ffc5e1c9cd8b47b82162596d123cf
Pete Jemian <prjemian@gmail.com>
2022-12-22 00:53:29 -0600

    MNT #62

--- pyRestTable/__init__.py
+++ pyRestTable/__init__.py
@@ -7,11 +7,13 @@ from .rest_table import Table
 try:
     from setuptools_scm import get_version
 
-    __version__ = get_version()
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
 except (LookupError, ModuleNotFoundError):
     from importlib.metadata import version
 
     __version__ = version("pyRestTable")
+    del version
 
 # -----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
