fdfe862ac2b7ae567dac946dd4ffbbdbc20b007b
Pete R Jemian <prjemian@gmail.com>
2024-10-01 15:46:46 -0500

    TST #83 matrix of tests

--- pyRestTable/tests/test_results.py
+++ pyRestTable/tests/test_results.py
@@ -17,7 +17,11 @@ import pytest
 import urllib.request
 
 
-CANSAS_URL = "https://raw.githubusercontent.com/" "canSAS-org/1dwg/master/" "examples/cs_af1410.xml"
+CANSAS_URL = (
+    "https://raw.githubusercontent.com/"
+    "canSAS-org/1dwg/master/"
+    "examples/cs_af1410.xml"
+)
 
 
 def cansas():
@@ -42,51 +46,319 @@ def cansas():
     return t
 
 
-def population_table():
+# These tables are in the documentation.
+complicated_labels = [
+    "Name\nand\nAttributes",
+    "Type",
+    "Units",
+    "Description\n(and Occurrences)",
+]
+complicated_rows = [
+    ["one,\ntwo", "buckle my", "shoe.\n\n\nthree,\nfour", "..."],
+    ["class", "NX_FLOAT", "", None],
+    list(range(0, 4)),
+    [None, {"a": 1, "b": "dreamy"}, 1.234, list(range(3))],
+]
+population_labels = ["City name", "Area", "Population", "Annual Rainfall"]
+population_rows = [
+    ["Adelaide", 1295, 1158259, 600.5],
+    ["Brisbane", 5905, 1857594, 1146.4],
+    ["Darwin", 112, 120900, 1714.7],
+]
+simple_labels = "one two three".split()
+simple_rows = [[f"{r},{c}" for c in [1, 2, 3]] for r in [1, 2, 3, 4]]
+
+
+@pytest.mark.parametrize(
+    "fmt, labels, rows, longtable, alignment, expected",
+    [
+        [
+            "simple",
+            simple_labels,
+            simple_rows,
+            False,
+            None,
+            (
+                "=== === =====\n"
+                "one two three\n"
+                "=== === =====\n"
+                "1,1 1,2 1,3\n"
+                "2,1 2,2 2,3\n"
+                "3,1 3,2 3,3\n"
+                "4,1 4,2 4,3\n"
+                "=== === ====="
+            ),
+        ],
+        [
+            "plain",
+            simple_labels,
+            simple_rows,
+            False,
+            None,
+            (
+                "one two three\n"
+                "1,1 1,2 1,3\n"
+                "2,1 2,2 2,3\n"
+                "3,1 3,2 3,3\n"
+                "4,1 4,2 4,3"
+            ),
+        ],
+        [
+            "grid",
+            simple_labels,
+            simple_rows,
+            False,
+            None,
+            (
+                "+-----+-----+-------+\n"
+                "| one | two | three |\n"
+                "+=====+=====+=======+\n"
+                "| 1,1 | 1,2 | 1,3   |\n"
+                "+-----+-----+-------+\n"
+                "| 2,1 | 2,2 | 2,3   |\n"
+                "+-----+-----+-------+\n"
+                "| 3,1 | 3,2 | 3,3   |\n"
+                "+-----+-----+-------+\n"
+                "| 4,1 | 4,2 | 4,3   |\n"
+                "+-----+-----+-------+"
+            ),
+        ],
+        [
+            "markdown",
+            simple_labels,
+            simple_rows,
+            False,
+            None,
+            (
+                "| one | two | three |\n"
+                "| --- | --- | ----- |\n"
+                "| 1,1 | 1,2 | 1,3   |\n"
+                "| 2,1 | 2,2 | 2,3   |\n"
+                "| 3,1 | 3,2 | 3,3   |\n"
+                "| 4,1 | 4,2 | 4,3   |"
+            ),
+        ],
+        [
+            "list-table",
+            simple_labels,
+            simple_rows,
+            False,
+            None,
+            (
+                ".. list-table::\n"
+                "   :header-rows: 1\n"
+                "   :widths: 3 3 5\n"
+                "\n"
+                "   * - one\n"
+                "     - two\n"
+                "     - three\n"
+                "   * - 1,1\n"
+                "     - 1,2\n"
+                "     - 1,3\n"
+                "   * - 2,1\n"
+                "     - 2,2\n"
+                "     - 2,3\n"
+                "   * - 3,1\n"
+                "     - 3,2\n"
+                "     - 3,3\n"
+                "   * - 4,1\n"
+                "     - 4,2\n"
+                "     - 4,3\n"
+            ),
+        ],
+        [
+            "html",
+            simple_labels,
+            simple_rows,
+            False,
+            None,
+            (
+                "<table>\n"
+                "  <tr>\n"
+                "    <th>one</th>\n"
+                "    <th>two</th>\n"
+                "    <th>three</th>\n"
+                "  </tr>\n"
+                "  <tr>\n"
+                "    <td>1,1</td>\n"
+                "    <td>1,2</td>\n"
+                "    <td>1,3</td>\n"
+                "  </tr>\n"
+                "  <tr>\n"
+                "    <td>2,1</td>\n"
+                "    <td>2,2</td>\n"
+                "    <td>2,3</td>\n"
+                "  </tr>\n"
+                "  <tr>\n"
+                "    <td>3,1</td>\n"
+                "    <td>3,2</td>\n"
+                "    <td>3,3</td>\n"
+                "  </tr>\n"
+                "  <tr>\n"
+                "    <td>4,1</td>\n"
+                "    <td>4,2</td>\n"
+                "    <td>4,3</td>\n"
+                "  </tr>\n"
+                "</table>\n"
+            ),
+        ],
+        [
+            "simple",
+            population_labels,
+            population_rows,
+            False,
+            None,
+            (
+                "========= ==== ========== ===============\n"
+                "City name Area Population Annual Rainfall\n"
+                "========= ==== ========== ===============\n"
+                "Adelaide  1295 1158259    600.5          \n"
+                "Brisbane  5905 1857594    1146.4         \n"
+                "Darwin    112  120900     1714.7         \n"
+                "========= ==== ========== ===============\n"
+            ),
+        ],
+        [
+            "plain",
+            population_labels,
+            population_rows,
+            False,
+            None,
+            (
+                "City name Area Population Annual Rainfall\n"
+                "Adelaide  1295 1158259    600.5          \n"
+                "Brisbane  5905 1857594    1146.4         \n"
+                "Darwin    112  120900     1714.7         \n"
+            ),
+        ],
+        [
+            "grid",
+            population_labels,
+            population_rows,
+            False,
+            None,
+            (
+                "+-----------+------+------------+-----------------+\n"
+                "| City name | Area | Population | Annual Rainfall |\n"
+                "+===========+======+============+=================+\n"
+                "| Adelaide  | 1295 | 1158259    | 600.5           |\n"
+                "+-----------+------+------------+-----------------+\n"
+                "| Brisbane  | 5905 | 1857594    | 1146.4          |\n"
+                "+-----------+------+------------+-----------------+\n"
+                "| Darwin    | 112  | 120900     | 1714.7          |\n"
+                "+-----------+------+------------+-----------------+\n"
+            ),
+        ],
+        [
+            "simple",
+            complicated_labels,
+            complicated_rows,
+            True,
+            None,
+            (
+                # ".. tabularcolumns:: |l|L|c|r|\n"
+                # "    :longtable:\n"
+                # "\n"
+                "========== ======================= ====== =================\n"
+                "Name       Type                    Units  Description      \n"
+                "and                                       (and Occurrences)\n"
+                "Attributes                                                 \n"
+                "========== ======================= ====== =================\n"
+                "one,       buckle my               shoe.  ...              \n"
+                "two                                                        \n"
+                "                                                           \n"
+                "                                   three,                  \n"
+                "                                   four                    \n"
+                "class      NX_FLOAT                       None             \n"
+                "0          1                       2      3                \n"
+                "None       {'a': 1, 'b': 'dreamy'} 1.234  [0, 1, 2]        \n"
+                "========== ======================= ====== ================="
+            ),
+        ],
+        [
+            "grid",
+            complicated_labels,
+            complicated_rows,
+            True,
+            None,
+            (
+                "+------------+-------------------------+--------+-------------------+\n"
+                "| Name       | Type                    | Units  | Description       |\n"
+                "| and        |                         |        | (and Occurrences) |\n"
+                "| Attributes |                         |        |                   |\n"
+                "+============+=========================+========+===================+\n"
+                "| one,       | buckle my               | shoe.  | ...               |\n"
+                "| two        |                         |        |                   |\n"
+                "|            |                         |        |                   |\n"
+                "|            |                         | three, |                   |\n"
+                "|            |                         | four   |                   |\n"
+                "+------------+-------------------------+--------+-------------------+\n"
+                "| class      | NX_FLOAT                |        | None              |\n"
+                "+------------+-------------------------+--------+-------------------+\n"
+                "| 0          | 1                       | 2      | 3                 |\n"
+                "+------------+-------------------------+--------+-------------------+\n"
+                "| None       | {'a': 1, 'b': 'dreamy'} | 1.234  | [0, 1, 2]         |\n"
+                "+------------+-------------------------+--------+-------------------+"
+            ),
+        ],
+        [
+            "list-table",
+            complicated_labels,
+            complicated_rows,
+            True,
+            None,
+            (
+                ".. list-table::\n"
+                "   :header-rows: 1\n"
+                "   :widths: 10 23 6 17\n"
+                "\n"
+                "   * - Name\n"
+                "       and\n"
+                "       Attributes\n"
+                "     - Type\n"
+                "     - Units\n"
+                "     - Description\n"
+                "       (and Occurrences)\n"
+                "   * - one,\n"
+                "       two\n"
+                "     - buckle my\n"
+                "     - shoe.\n"
+                "\n"
+                "\n"
+                "       three,\n"
+                "       four\n"
+                "     - ...\n"
+                "   * - class\n"
+                "     - NX_FLOAT\n"
+                "     -\n"
+                "     -\n"
+                "   * - 0\n"
+                "     - 1\n"
+                "     - 2\n"
+                "     - 3\n"
+                "   * - None\n"
+                "     - {'a': 1, 'b': 'dreamy'}\n"
+                "     - 1.234\n"
+                "     - [0, 1, 2]"
+            ),
+        ],
+    ],
+)
+def test_parametrized(fmt, labels, rows, longtable, alignment, expected):
+    assert isinstance(longtable, bool)
+
     t = Table()
-    t.addLabel("City name")
-    t.addLabel("Area")
-    t.addLabel("Population")
-    t.addLabel("Annual Rainfall")
-    t.addRow(["Adelaide", 1295, 1158259, 600.5])
-    t.addRow(["Brisbane", 5905, 1857594, 1146.4])
-    t.addRow(["Darwin", 112, 120900, 1714.7])
-    return t
-
-
-def test_simple():
-    s = population_table().reST(fmt="simple")
-    expected = "========= ==== ========== ===============\n"
-    expected += "City name Area Population Annual Rainfall\n"
-    expected += "========= ==== ========== ===============\n"
-    expected += "Adelaide  1295 1158259    600.5          \n"
-    expected += "Brisbane  5905 1857594    1146.4         \n"
-    expected += "Darwin    112  120900     1714.7         \n"
-    expected += "========= ==== ========== ===============\n"
-    assert s == expected
-
-
-def test_plain():
-    s = population_table().reST(fmt="plain")
-    expected = "City name Area Population Annual Rainfall\n"
-    expected += "Adelaide  1295 1158259    600.5          \n"
-    expected += "Brisbane  5905 1857594    1146.4         \n"
-    expected += "Darwin    112  120900     1714.7         \n"
-    assert s == expected
-
-
-def test_grid():
-    s = population_table().reST(fmt="grid")
-    expected = "+-----------+------+------------+-----------------+\n"
-    expected += "| City name | Area | Population | Annual Rainfall |\n"
-    expected += "+===========+======+============+=================+\n"
-    expected += "| Adelaide  | 1295 | 1158259    | 600.5           |\n"
-    expected += "+-----------+------+------------+-----------------+\n"
-    expected += "| Brisbane  | 5905 | 1857594    | 1146.4          |\n"
-    expected += "+-----------+------+------------+-----------------+\n"
-    expected += "| Darwin    | 112  | 120900     | 1714.7          |\n"
-    expected += "+-----------+------+------------+-----------------+\n"
-    assert s == expected
+    t.labels = labels
+    for row in rows:
+        assert len(row) == len(labels)
+        t.addRow(row)
+    t.setLongtable(longtable)
+    if alignment is not None:
+        t.setTabularColumns(state=True, column_spec=alignment)
+    s = t.reST(fmt=fmt)
+    assert len(s.strip().splitlines()) == len(expected.splitlines())
+    for r, e in zip(s.strip().splitlines(), expected.splitlines()):
+        assert r.rstrip() == e.rstrip(), f"{r=!r}, {e=!r}"
 
 
 @pytest.mark.parametrize(
@@ -201,9 +473,45 @@ def test_default_str():
 def test_issue_31():
     table = Table()
     table.labels = ["h", "k", "l", "mu", "omega", "chi", "phi", "gamma", "delta"]
-    table.addRow([0, 1, 1, 0.0, 3.4824458166048444, 22.712897698011936, 0.0, 0.0, 13.799774663132288])
-    table.addRow([0.0, 8.0, 0.0, 0.0, 22.31594087562736, 89.13769999977886, 0.0, 0.0, 45.158571742842376])
-    table.addRow([0.0, 12.0, 1.0, 0.0, 34.963469180020944, 78.33265876350477, 0.0, 0.0, 71.80070421791422])
+    table.addRow(
+        [
+            0,
+            1,
+            1,
+            0.0,
+            3.4824458166048444,
+            22.712897698011936,
+            0.0,
+            0.0,
+            13.799774663132288,
+        ]
+    )
+    table.addRow(
+        [
+            0.0,
+            8.0,
+            0.0,
+            0.0,
+            22.31594087562736,
+            89.13769999977886,
+            0.0,
+            0.0,
+            45.158571742842376,
+        ]
+    )
+    table.addRow(
+        [
+            0.0,
+            12.0,
+            1.0,
+            0.0,
+            34.963469180020944,
+            78.33265876350477,
+            0.0,
+            0.0,
+            71.80070421791422,
+        ]
+    )
 
     result = str(table).splitlines()
 
